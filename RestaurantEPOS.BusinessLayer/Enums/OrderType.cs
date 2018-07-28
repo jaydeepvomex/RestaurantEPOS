2b5517fd4037b3444f971993d546313c3c356b25
Krunal <p13205072@my365.dmu.ac.uk>
2018-07-28 18:03:31 +0100

    Added Business Layer and create order type enum

---
+++ RestaurantEPOS.BusinessLayer/Enums/OrderType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantEPOS.BusinessLayer
+{
+    public enum OrderType
+    {
+        TableService,
+        Takeaway,
+        Delivery
+    }
+}
